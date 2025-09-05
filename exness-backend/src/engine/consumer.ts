364536a66b6052b39fb404f0d7600cdc06f44d29
Pratik Khaire <Pratik.shekhar.khaire@gmail.com>
2025-09-05 22:10:59 +0530

    flex: {the error of connection}

--- exness-backend/src/engine/consumer.ts
+++ exness-backend/src/engine/consumer.ts
@@ -4,28 +4,28 @@ const kafka = new Kafka({
   brokers: ["localhost:9092"],
 });
 
-const consumer = kafka.consumer({groupId:'trading-engin-group'});
+const consumer = kafka.consumer({ groupId: "trading-engin-group" });
 
-const KAFKA_TOPIC= 'backpack-market-update';
+const KAFKA_TOPIC = "backpack-market-updates";
 
-export const connectConsumer = async () =>{
-    try{
-        await consumer.connect();
-        await consumer.subscribe({topic:KAFKA_TOPIC,fromBeginning:true});
-        await consumer.run({
-            eachMessage:async ({ topic,partition,message}) =>{
-                if(!message|| !message.value){
-                    return;
-                }
-                const messageValueString = message.value.toString();
+export const connectConsumer = async () => {
+  try {
+    await consumer.connect();
+    await consumer.subscribe({ topic: KAFKA_TOPIC, fromBeginning: true });
+    await consumer.run({
+      eachMessage: async ({ topic, partition, message }) => {
+        if (!message || !message.value) {
+          return;
+        }
+        const messageValueString = message.value.toString();
 
-                const payload = JSON.parse(messageValueString);
+        const payload = JSON.parse(messageValueString);
 
-                console.log('[Engine] received new market data',payload);
-            },
-        });
-    } catch(error){
-        console.log('Erro in kafka consumer',error);
-        process.exit(1);
-    }
+        console.log("[Engine] received new market data", payload);
+      },
+    });
+  } catch (error) {
+    console.log("Erro in kafka consumer", error);
+    process.exit(1);
+  }
 };
--- exness-backend/src/index.ts
+++ exness-backend/src/index.ts
@@ -1,26 +1,25 @@
+import express from "express";
+import { connectProduct } from "./services/kafka.producer";
 
-import  express  from "express";
-import { connectProduct } from './services/kafka.producer';
-
-import { connectToBackpackStream } from './services/backpack.service';
-import { connectConsumer } from './engine/consumer';
+import { connectToBackpackStream } from "./services/backpack.service";
+import { connectConsumer } from "./engine/consumer";
 
 console.log(`staring the applicaiton`);
 
-const startKafka = async () =>{
-    console.log('Initialing kafka');
-    await connectProduct();
+const startKafka = async () => {
+  console.log("Initialing kafka");
+  await connectProduct();
 
-    console.log('Initializing kafka consumer');
-    await connectConsumer();
+  console.log("Initializing kafka consumer");
+  await connectConsumer();
 
-    console.log('Kafka service ready');
-    connectToBackpackStream();
+  console.log("Kafka service ready");
+  connectToBackpackStream();
 };
 
-startKafka().catch(error=>{
-    console.log("A error in the startKafka",error);
-    process.exit(1);
+startKafka().catch((error) => {
+  console.log("A error in the startKafka", error);
+  process.exit(1);
 });
 
 const app = express();
@@ -28,16 +27,13 @@ const PORT = 4000;
 
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send('welcome to the express server')
+app.get("/", (req, res) => {
+  res.send("welcome to the express server");
 });
 
-app.listen(PORT,()=>{
-    console.log(`Express server started at the ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Express server started at the ${PORT}`);
 });
-
 
 // function express() {
 //   throw new Error("Function not implemented.");
-
-
